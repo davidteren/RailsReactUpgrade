f38df6afb594290111af6bde87aaa7dba1170634
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2024-02-28 18:16:41 -0500

    Resolve MODULE_LEVEL_DIRECTIVE warnings during vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import RubyPlugin from "vite-plugin-ruby";
 export default defineConfig({
   plugins: [RubyPlugin()],
   optimizeDeps: {},
+  build: {
+    rollupOptions: {
+      // Safely ignores MODULE_LEVEL_DIRECTIVE warnings https://github.com/TanStack/query/issues/5175#issuecomment-1499120399
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 });
