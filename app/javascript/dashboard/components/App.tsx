2c5a0068f797ca3d1cf5216ae93277203a5bd5d8
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2024-03-01 19:14:08 -0500

    Add image imports from app/assets path

--- app/javascript/dashboard/components/App.tsx
+++ app/javascript/dashboard/components/App.tsx
@@ -3,6 +3,9 @@ import { Container, Row, Col, Card, Alert} from "react-bootstrap"
 import CustomForm  from  "./DashboardForm"
 // import { App } from "@/components/ReactLoader"
 
+import image1 from "@assets/images/image_1.jpeg"
+import image2 from "@assets/images/image_2.png"
+
 const Dashboard = () => {
   // Hardcoded data for the list widget
   const dataList = [
@@ -56,6 +59,9 @@ const Dashboard = () => {
       <Alert variant="info">
         A simple info alert — check it out!
       </Alert>
+
+      <img src={image1} />
+      <img src={image2} />
     </Container>
   )
 }
